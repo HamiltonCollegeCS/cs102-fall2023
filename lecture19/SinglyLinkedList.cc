953cca11a1e2f4ec1fc6a1e85f9f2bc9ffd2b321
Darren Strash <darrenstrash@users.noreply.github.com>
2023-10-17 15:58:54 +0000

    Lecture 19

--- lecture19/SinglyLinkedList.cc
+++ lecture19/SinglyLinkedList.cc
@@ -25,10 +25,17 @@ class SinglyLinkedList {
 public:
     SinglyLinkedList();
     Node * get_node(int i);
+    void set(int i, int x);
+    int get(int i);
+    void insert(int i, int x);
 private:
     Node * _head;
     Node * _tail;
 };
+
+SinglyLinkedList::SinglyLinkedList() {
+    _head = nullptr;
+}
 
 Node * SinglyLinkedList::get_node(int i) {
     // Plan:
@@ -37,6 +44,26 @@ Node * SinglyLinkedList::get_node(int i) {
     //
     //}
     return current;
+}
+
+void SinglyLinkedList::set(int i, int x) {
+    get_node(i)->_data = x;
+}
+
+int SinglyLinkedList::get(int i) {
+    return get_node(i)->_data;
+}
+
+void SinglyLinkedList::insert(int i, int x) {
+    Node * baby = new Node(x);
+    if (i == 0) {
+        baby->_next = _head;
+        _head = baby;
+    } else {
+        Node * before = get_node(i - 1);
+        baby->_next = before->_next;
+        before->_next = baby;
+    }
 }
 
 // executes program
